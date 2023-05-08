db8c88599b52222743f115b697f931c31adcb651
Rasa <rasa.kajackaite@gmail.com>
2023-05-08 14:56:12 +0300

    Created BookService and BookServiceImpl classes

---
+++ src/main/java/iAmDew/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,20 @@
+package iAmDew.spring6webapp.services;
+
+import iAmDew.spring6webapp.domain.Book;
+import iAmDew.spring6webapp.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Iterable<Book> findAll() {
+        return null;
+    }
+}
